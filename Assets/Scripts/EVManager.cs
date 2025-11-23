041baf5857d170b04c029eaa79de2519085a4d3b
Aterilus <terilusartavius@gmail.com>
2025-11-23 14:02:12 -0500

    Enemy  behavior
    deleted some of the player's behavior scripts and made an enemy to attack enemy behavior

---
+++ Assets/Scripts/EVManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class EVManager : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class StatsComponent
+{
+    public int maxHP = 0;
+    public int attack = 0;
+    public int defense = 0;
+    public int speed = 0;
+    public int currentHP = 0;
+
+}
